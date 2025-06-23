e815cc52f6946556996b20e79c9bc19a12e62a13
sauronmesh <saksham@unmeshed.com>
2025-06-24 00:30:48 +0530

    refactor: Minor doc improvement and fixes

--- examples/single/single-worker-main.go
+++ examples/single/single-worker-main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
-	apis "github.com/unmeshed/unmeshed-go-sdk/sdk/apis/main"
-	apis2 "github.com/unmeshed/unmeshed-go-sdk/sdk/apis/workers"
-	"github.com/unmeshed/unmeshed-go-sdk/sdk/configs"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
+
+	apis "github.com/unmeshed/unmeshed-go-sdk/sdk/apis/main"
+	apis2 "github.com/unmeshed/unmeshed-go-sdk/sdk/apis/workers"
+	"github.com/unmeshed/unmeshed-go-sdk/sdk/configs"
 )
 
 var unmeshedClient *apis.UnmeshedClient
@@ -21,13 +22,11 @@ func TestWorker(data map[string]interface{}) string {
 }
 
 func main() {
-	// Create a single worker
 	worker := apis2.NewWorker(TestWorker, "test-worker")
 
-	// Configure the client
 	clientConfig := configs.NewClientConfig()
-	clientConfig.SetClientID("0d9ea975-da46-4bff-b4b9-96be0f8a11b0")
-	clientConfig.SetAuthToken("MAoL7MFfKagCFtEW2DUS")
+	clientConfig.SetClientID("<< Client Id >>")
+	clientConfig.SetAuthToken("<< Auth Token >>")
 	clientConfig.SetDelayMillis(40)
 	clientConfig.SetPort(8080)
 	clientConfig.SetWorkRequestBatchSize(500)
@@ -35,7 +34,6 @@ func main() {
 	clientConfig.SetStepTimeoutMillis(36000000)
 	clientConfig.SetMaxWorkers(100)
 
-	// Create the Unmeshed client
 	var err error
 	unmeshedClient, err = apis.NewUnmeshedClient(clientConfig)
 	if err != nil {
@@ -43,13 +41,10 @@ func main() {
 		return
 	}
 
-	// Register the single worker
 	unmeshedClient.RegisterWorker(worker)
 
-	// Create a channel to signal when the client is done
 	done := make(chan struct{})
 
-	// Start the client in a goroutine
 	go func() {
 		unmeshedClient.Start()
 		close(done)
@@ -57,11 +52,9 @@ func main() {
 
 	fmt.Println("Single worker client started. Press Ctrl+C to stop...")
 
-	// Set up signal handling for graceful shutdown
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
 
-	// Wait for either a shutdown signal or for the client to finish
 	select {
 	case <-sigChan:
 		fmt.Println("\nReceived shutdown signal. Stopping client...")
