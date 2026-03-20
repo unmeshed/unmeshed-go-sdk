bc72daa262feb09a13d2a40d18c38dd7cadb47a9
Boney <boneysekh@gmail.com>
2026-03-20 01:49:38 -0700

    Add support for custom TLS and also to skip verification if needed

--- examples/single/single-worker-main.go
+++ examples/single/single-worker-main.go
@@ -248,8 +248,8 @@ func main() {
 	clientConfig.SetPort(8443)
 	clientConfig.SetWorkRequestBatchSize(500)
 	clientConfig.SetBaseURL("https://localhost:8443/")
-	clientConfig.SetDisableSSLVerification(true)                              // optional for local/self-signed TLS setups
-	clientConfig.SetCACertDirectory("/Users/boneysekh/Downloads/localcerts/") // optional directory containing custom .crt or .pem files
+	clientConfig.SetDisableSSLVerification(true)    // optional for local/self-signed TLS setups
+	clientConfig.SetCACertDirectory("/localcerts/") // optional directory containing custom .crt or .pem files
 	clientConfig.SetStepTimeoutMillis(36000000)
 	clientConfig.SetMaxWorkers(100)
 
